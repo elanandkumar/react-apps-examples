2d6c70941e263b9649854499253c633699b0077c
Anand Kumar <el.anand.kumar@gmail.com>
2021-07-02 21:37:30 +0530

    Added gemfile and fixed bundle issue

---
+++ examples/robofriends/precache-manifest.45934ebc3a07e6164cada5a854b60311.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "54ae542a300be3a2e27f",
+    "url": "./static/css/main.e3e91fe2.chunk.css"
+  },
+  {
+    "revision": "54ae542a300be3a2e27f",
+    "url": "./static/js/main.89e9640d.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "ea638720e647eb25c4e6",
+    "url": "./static/css/2.1299344f.chunk.css"
+  },
+  {
+    "revision": "ea638720e647eb25c4e6",
+    "url": "./static/js/2.366ff823.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "./static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "ddd65ab69f2a75671d4776ee10feac6b",
+    "url": "./index.html"
+  }
+];
--- examples/robofriends/service-worker.js
+++ examples/robofriends/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7da3364bb758a177ec42b803047691d8.js"
+  "./precache-manifest.45934ebc3a07e6164cada5a854b60311.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
